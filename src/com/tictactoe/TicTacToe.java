74172338e1932d77257cecc617bb6169340007b7
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-10-06 18:15:46 +0530

    [Dipali] Added : show board

--- src/com/tictactoe/TicTacToe.java
+++ src/com/tictactoe/TicTacToe.java
@@ -12,10 +12,10 @@ public class TicTacToe {
 	static char[] board = new char[10];
 
 	/**
-	 * @description
+	 * Creating the game board
 	 * 
 	 */
-	static void createBoard() {
+	private static void createBoard() {
 		System.out.println("TicTacToe game board");
 		for (int i = 1; i < board.length; i++) {
 			board[i] = ' ';
@@ -23,25 +23,42 @@ public class TicTacToe {
 	}
 
 	/**
-	 * @taking input from user to choose the turn
+	 * taking input from user to choose the turn
 	 */
-	static void chooseLetter() {
+	private static void chooseLetter() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the character X or O to choose the turn:");
-		char ch = sc.next().charAt(0);
-		if (ch == 'X' || ch == 'x') {
+		char turn = sc.next().charAt(0);
+		if (turn == 'X' || turn == 'x') {
 			System.out.println("player's turn to play the game");
-		} else if (ch == 'O' || ch == 'o') {
+			turn = 'O';
+		} else if (turn == 'O' || turn == 'o') {
 			System.out.println("computer's turn to play the game");
+			turn = 'X';
 		} else {
 			System.out.println("Invalid input");
 		}
 
 	}
-public static void main(String args[]) {
-		
+
+	/**
+	 * Displaying the tictactoe game board
+	 */
+	public static void showBoard() {
+
+		System.out.println("| " + board[1] + " | " + board[2] + " | " + board[3] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[4] + " | " + board[5] + " | " + board[6] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[7] + " | " + board[8] + " | " + board[9] + " |");
+
+	}
+
+	public static void main(String args[]) {
+
 		createBoard();
 		chooseLetter();
-		
+		showBoard();
+
 	}
-}		
+}
