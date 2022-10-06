5e7cba365caa1607e25a2b2e42e5f45a52929296
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-10-06 18:12:28 +0530

    [Dipali] Added : choose player

--- src/com/tictactoe/TicTacToe.java
+++ src/com/tictactoe/TicTacToe.java
@@ -1,4 +1,7 @@
 package com.tictactoe;
+
+import java.util.Scanner;
+
 /**
  * 
  * @author Dipali
@@ -9,21 +12,36 @@ public class TicTacToe {
 	static char[] board = new char[10];
 
 	/**
-	 * Description of the board
+	 * @description
 	 * 
 	 */
-	public static void createBoard() {
+	static void createBoard() {
 		System.out.println("TicTacToe game board");
-		for (int i = 1; i < 10; i++) {
+		for (int i = 1; i < board.length; i++) {
 			board[i] = ' ';
 		}
 	}
 
-	public static void main(String args[]) {
-
-		createBoard();
+	/**
+	 * @taking input from user to choose the turn
+	 */
+	static void chooseLetter() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the character X or O to choose the turn:");
+		char ch = sc.next().charAt(0);
+		if (ch == 'X' || ch == 'x') {
+			System.out.println("player's turn to play the game");
+		} else if (ch == 'O' || ch == 'o') {
+			System.out.println("computer's turn to play the game");
+		} else {
+			System.out.println("Invalid input");
+		}
 
 	}
-}
-
-
+public static void main(String args[]) {
+		
+		createBoard();
+		chooseLetter();
+		
+	}
+}		
